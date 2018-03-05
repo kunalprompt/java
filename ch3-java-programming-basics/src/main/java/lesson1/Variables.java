6ed682d70631a4de490c2cd72fc115e9914e152d
kunal.sharma <kunal.sharma@unbxd.com>
2018-03-05 20:10:27 +0530

    feat(Chapter3) - java variables and memory constructs

--- ch3-java-programming-basics/src/main/java/lesson1/Variables.java
+++ ch3-java-programming-basics/src/main/java/lesson1/Variables.java
@@ -6,6 +6,11 @@ public class Variables{
     private double numDouble;
     private String name;
 
+    // check the magics -  data types are the storage space - values stored in them
+    // ASCII conversions happen here while formatting the data (like conversion to character).
+    private int assign = 'c';
+    private char a = 65;
+
     public Variables(int number, float numFloat, double numDouble, String name){
         this.number = number;
         this.numFloat = numFloat;
@@ -14,6 +19,7 @@ public class Variables{
     }
 
     public String printData(){
+        System.out.println("See the difference here --- " + assign + " -- " + a);
         return "Number: " + number + " Float: " + numFloat + " Double: " + numDouble + " Name: " + name;
     }
 }
